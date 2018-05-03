14fd6a5b2dd9a3197f399c455904edff164346cb
ydennisy <dennis.yurkevich@gmail.com>
2018-05-03 14:12:40 +0100

    webpack for html

--- client/modal.js
+++ client/modal.js
@@ -1,25 +1,14 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import '../node_modules/uikit/dist/css/uikit.min.css';
-
+import Popup from './templates/popup.html'
 // loads the Icon plugin
 UIkit.use(Icons);
-
-const html = `
-        <div id="123" class="uk-modal-body" bg-close="false">
-            <h2 class="uk-modal-title">We use cookies!</h2>
-            <p>There is new regulation in the EU which requires you to consent to our use of your personal data. We and our partners collect and use cookies for ad personalisation and measurement. </p>
-            <p class="uk-text-right">
-            <button id="information-button" class="uk-button uk-button-default uk-modal-close" type="button">More Information</button>
-            <button id="agree-button" class="uk-button uk-button-primary uk-modal-close" type="button">Agree</button>
-            </p>
-        </div>
-        `
 
 // this should return the user selection as a resolved promise
 function showConsentModal () {
     
-    UIkit.modal.dialog(html, { 'bg-close'  : false });
+    UIkit.modal.dialog(Popup);
     
     document.getElementById('agree-button').addEventListener('click', function () {
         //console.log('CMP => User consent given')
