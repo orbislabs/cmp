011a0903d6b2718a3da996c848e082d9b7c7a5e4
ydennisy <dennis.yurkevich@gmail.com>
2018-05-16 10:11:07 +0100

    added dev/prod profiles

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-// check node v9.8.0
-console.log(`Node:  ${process.version}`);
-
+// load node server modules
+const os = require('os');
 // setup express
 const express = require('express');
 const app = express();
+
+// config based on DEV or PROD profiles
+const host = os.networkInterfaces().lo0[0].address;
+let PORT;
+let HOST;
+
+if (host == '127.0.0.1') {
+    PORT = 3999;
+    HOST = 'localhost';
+    process.env.NODE_ENV = 'development';
+} else {
+    PORT = 3999;
+    HOST = host;
+    process.env.NODE_ENV = 'production';
+}
 
 // self rolled modules imported here
 const cmp = require('./routes/cmp');
@@ -17,6 +31,10 @@ app.use(express.static('dist'));
 app.use(express.static('/node_modules/uikit/dist/css/')); // TODO: try to remove
 
 // fire up the server
-app.listen(3999, () => { 
-    console.log('CMP++ :: ExpressServer --> listening on PORT : 3999');
+app.listen(PORT, () => { 
+    console.log(`CMP++ :: ExpressServer --> Running on ${HOST} : ${PORT}`);
 });
+// check node v9.8.0
+console.log(`Node:  ${process.version}`);
+console.log(`Server IP:  ${host}`);
+console.log(`Env:  ${JSON.stringify(process.env.NODE_ENV)}`);
