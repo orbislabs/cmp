365a0c85b0f974d3fc10b9715b679ae0907be6ca
Dennis Yurkevich <dennis@mediaiqdigital.com>
2018-06-03 20:34:49 +0100

    bug

--- routes/cmp.js
+++ routes/cmp.js
@@ -6,27 +6,63 @@ const rootPath = path.join(__dirname, '../dist');
 const public = path.join(__dirname, '../public');
 
 // countries
-const euCountries = require('./countries.js');
-
+//const euCountries = require('./countries.js');
+const euCountries = [
+  'GB',
+  'AT',
+  'BE',
+  'BG',
+  'HR',
+  'CY',
+  'CZ',
+  'DK',
+  'EE',
+  'FI',
+  'FR',
+  'DE',
+  'GR',
+  'HU',
+  'IE',
+  'IT',
+  'LV',
+  'LT',
+  'LU',
+  'MT',
+  'NL',
+  'PL',
+  'PT',
+  'RO',
+  'SK',
+  'SI',
+  'ES',
+  'SE'
+];
 // middleware logging a request to this route
 router.use(function timeLog(req, res, next) {
   //TODO : remove logging in prod.
   console.log('GET /cmp @ Time : ', Date.now());
-  console.log(req.get('X-Request-Country'));
-  if (euCountries.indexOf(req.get('X-Request-Country')) < 0) {
+  //console.log(req.get('X-Request-Country'));
+/*   if (euCountries.indexOf(req.get('X-Request-Country')) < 0) {
     console.log('non-eu');
-  }
+  } */
+  console.log(typeof euCountries)
   next();
 });
 
 router.get('/', (req, res) => {
-  if (euCountries.indexOf(req.get('X-Request-Country')) >= 0) {
+  if(req.hostname !== 'localhost') {
+    if (euCountries.indexOf(req.get('X-Request-Country')) >= 0) {
+      res.sendFile('cmp.bundle.js', {
+        root : rootPath
+      });
+    } else {
+      res.sendFile('gtmConsent.js', {
+        root : public
+      });
+    }
+  } else {
     res.sendFile('cmp.bundle.js', {
       root : rootPath
-    });
-  } else {
-    res.sendFile('gtmConsent.js', {
-      root : public
     });
   }
 });
