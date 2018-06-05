c0a0707e04a8bfc074cd87a7e1022010bfc60e20
ydennisy <dennis.yurkevich@gmail.com>
2018-06-05 18:45:15 +0100

    cookies set in headers

--- client/cookies.js
+++ client/cookies.js
@@ -84,7 +84,7 @@ function readCookie(name = 'euconsent') {
 
 function requestHttpCookies (cookieName, cookieValue) {
   return new Promise((resolve, reject) => {
-    fetch(`http://pluto-cmp.com/cmp/cookie?n=${cookieName}&c=${cookieValue}`)
+    fetch(`http://pluto-cmp.com:5000/cmp/cookie?n=${cookieName}&c=${cookieValue}`)
       .then((response) => { console.log(response); })
       .catch((err) => { console.error(err) ; });
     resolve(true);
