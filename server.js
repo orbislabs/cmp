cea5975d64520a29a12096735498f86c13720ebe
ydennisy <dennis.yurkevich@gmail.com>
2018-06-07 15:21:30 +0100

    more http cookies debugging

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-process.env.PORT_PRODUCTION = 'production'
+//process.env.PORT_PRODUCTION = 'production'
 // load node server modules
 const os = require('os');
 // setup express 
