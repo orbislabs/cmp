f154535090447fc1fa9d43992b158c63d48aa39d
Dennis Yurkevich <dennis@mediaiqdigital.com>
2018-06-20 08:47:55 +0100

    -main.js moved

--- client/main.js
+++ client/main.js
@@ -1,21 +1,40 @@
-import './refactor.main';
+import initLoader from './loader/index';
+import initCmp from './cmp/index';
+import initApi from './api/index';
+import isShowUi from './cmp/isShowUi';
 
-// Main CMP flow logic is here.
-/* init()
-  .then(result => cookies.checkCookiesEnabledPromise(result)) // true OR false
-  .then(result => cookies.checkIabCookie(result)) // base64 OR false
-  .then(result => loadCmp(clientId, iabVendorList, result))
-  .then((result) => {
-    if (result == false) {
-      renderVueApp(clientId)
+async function init() {
+  const loaderData = await initLoader();
+  initCmp(loaderData)
+    .then(cmp => initApi(cmp))
+    .then(() => isShowUi(loaderData.iabCookie))
+    .then((result) => {
+      if (!result) {
+        return import(/* webpackChunkName: "ui" */ './ui/main.js')
+          .then(appModule => appModule.default(loaderData.clientId));
+      }
+      return Promise.resolve();
+    })
+    .catch(err => console.error(err));
+}
+init();
+
+/*
+
         .then(result => cmp.updateCmpAndWriteCookie(result))
         .then(result => fireGtmPixels(clientId))
         .then(result => cmp.readyCmpAPI(result))
         .then(result => cookies.requestHttpCookies( 'euconsent', cmp.getConsentString() ))
-		    .catch(err => console.log(err));
-    } else {
+        .catch(err => console.log(err));
+
+
+          } else {
       fireGtmPixels(clientId)
         .then(result => cmp.readyCmpAPI(result));
-    }
-  })
-  .catch(err => console.log(err)); */
+----------------------
+-- isShowUi returns a boolean
+-- Cmp class should have an interface for showing the UI
+-- it can also accept a bool, and exit if false
+-- otherwise it can render the UI via first using the import() call.
+-- subsequent calls to showConsentTool(), may need to avoid calling import() again?
+*/
