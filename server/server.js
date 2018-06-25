ed28576ff75a84e7e3666316127e4ccf43f4ba17
ydennisy <dennis.yurkevich@gmail.com>
2018-06-25 13:15:41 +0100

    added env vars for staging

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 require('dotenv').config();
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cmp = require('./routes/cmp');
 const api = require('./routes/api');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-const PORT = process.env.PORT_PRODUCTION || 5000; // .env is not working
+if (process.env.NODE_ENV === 'production') {
+  process.env.PORT = process.env.PORT_PRODUCTION || 5000;
+} else if (process.env.NODE_ENV === 'staging') {
+  process.env.PORT = process.env.PORT_STAGING || 6000;
+} else {
+  process.env.PORT = process.env.PORT_DEVELOPMENT || 5000;
+}
+
 const app = express();
 
 app.use(cookieParser());
@@ -15,8 +23,8 @@ app.use(express.static('../dist'));
 app.use(express.static('../public'));
 
 // fire up the server
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`CMP++ :: ExpressServer --> Mode : ${process.env.NODE_ENV}`);
-  console.log(`CMP++ :: ExpressServer --> NodeV :  ${process.version}`); // node v9.8.0 -> needs to be updated
-  console.log(`CMP++ :: ExpressServer --> Port : ${PORT}`);
+  console.log(`CMP++ :: ExpressServer --> NodeV :  ${process.version}`); // TODO: node v9.8.0 -> needs to be updated
+  console.log(`CMP++ :: ExpressServer --> Port : ${process.env.PORT}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -9,74 +9,79 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
-const isProduction = process.env.NODE_ENV === 'production'
-const PRODUCTION_HOST = 'https://pluto.mgr.consensu.org/'
+const isProduction = process.env.NODE_ENV === 'production';
+const isStaging = process.env.NODE_ENV === 'staging';
+
+const PRODUCTION_HOST = 'https://pluto.mgr.consensu.org/'; // TODO: where is this?
+
 const config = {
   entry: {
     cmp: ['babel-polyfill', './client/main.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath : isProduction ? PRODUCTION_HOST : '/',
+    publicPath: isProduction ? PRODUCTION_HOST : '/',
     filename: '[name].bundle.js',
     chunkFilename: '[name].chunk.bundle.js',
 
   },
-  resolve : {
-    alias : { '@': path.resolve('client')}
+  resolve: {
+    alias: {
+      '@': path.resolve('client'),
+    },
   },
   module: {
     rules: [{
-        test: /\.vue$/,
-        loader: 'vue-loader'
+      test: /\.vue$/,
+      loader: 'vue-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader',
       },
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-        }
+        loader: 'css-loader',
       },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-          }]
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+        },
+      ],
+    },
+    {
+      test: /\.(html)$/,
+      use: {
+        loader: 'html-loader',
       },
-      {
-        test: /\.scss$/,
-        use: [
-            'vue-style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader'
-            }
-          ]
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader'
-        }
-      }
-    ]
+    },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
-    new CleanWebpackPlugin('dist', {} ),
+    new CleanWebpackPlugin('dist', {}),
     new HtmlWebpackPlugin({
       template: './build/template.index.html'
     }),
     new HtmlWebpackExcludeAssetsPlugin(),
     new Dotenv(),
     new BundleAnalyzerPlugin({
-      analyzerMode: (isProduction) ? 'disabled' : 'server' 
-    })
-  ]
+      analyzerMode: (isProduction) ? 'disabled' : 'server'
+    }),
+  ],
 };
 
 module.exports = config;
