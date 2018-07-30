04510a037ebf11b8648c29695533b230394032ec
ydennisy <dennis.yurkevich@gmail.com>
2018-07-30 16:14:34 +0100

    small refactor on non-eu GTM

--- public/gtmConsent.js
+++ public/gtmConsent.js
@@ -1,6 +1,10 @@
-if (typeof dataLayer === 'undefined') {
-    console.log('dataLayer is undefined');
+if (typeof window.dataLayer === 'undefined') {
+  console.log('[INFO][Module-TMS] dataLayer is undefined.');
 } else {
-    dataLayer.push({'consentStatus':'true'});
-    dataLayer.push({'event':'consentUpdate'}); 
+  document.cookie = 'isFunctionalAllowed=true';
+  document.cookie = 'isAnalyticsAllowed=true';
+  document.cookie = 'isMarketingAllowed=true';
+  window.dataLayer.push({
+    event: 'updatedConsentSettings',
+  });
 }
