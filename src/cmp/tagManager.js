894a0bc6b3d1adbe478fdc8e24f3341221c8c030
ydennisy <dennis.yurkevich@gmail.com>
2018-07-31 15:23:37 +0100

    deleted commented unused function

--- src/cmp/tagManager.js
+++ src/cmp/tagManager.js
@@ -5,7 +5,7 @@ map.set(11, 'isFunctionalAllowed');
 map.set(12, 'isAnalyticsAllowed');
 map.set(13, 'isMarketingAllowed');
 
-function updateConsentCookie(purposeArray) {
+function updateConsentCookies(purposeArray) {
   for (let i = 0; i < purposeArray.length; i++) {
     if (map.get(purposeArray[i])) {
       Cookie.set(map.get(purposeArray[i]), true);
@@ -17,14 +17,14 @@ export default function tagManagerModule() {
   return new Promise((resolve, reject) => {
     const purposeArray = cmp.getPurposesAllowed();
     if (purposeArray === undefined || purposeArray.length == 0) {
-      console.log('[INFO][Module-TMS] => No consented purposes');
+      console.log('[INFO][Module-TMS] => No user consented purposes');
       resolve(false);
     } else if (!window.dataLayer) {
       console.log('[INFO][Module-TMS] => No dataLayer Detected');
       resolve(false);
     } else {
       console.log('[INFO][Module-TMS] => Consented purposes', purposeArray);
-      updateConsentCookie(purposeArray);
+      updateConsentCookies(purposeArray);
       window.dataLayer.push({ event: 'updatedConsentSettings' });
       console.log('[INFO][Module-TMS] => Updated dataLayer', window.dataLayer);
       resolve(true);
